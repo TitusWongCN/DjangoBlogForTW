4f3afeef1e9e665793f234a03787376ae96b9dd3
TitusWongCN <wzhwno1@163.com>
2019-09-25 21:00:40 +0800

    create blog's db model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=70)
+    create_time = models.DateTimeField()
+    modify_time = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    abstract = models.CharField(max_length=200, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, blank=True)
+
